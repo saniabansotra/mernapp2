9ed8328a8e67d18ea4e04e6e8fe78e735f590834
Sania Bansotra <saniabansotra@gmail.com>
2023-08-14 18:41:29 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import Home from "./component/index";
+import Home from "./component/home";
+import Navbar from "./component/navbar";
 
 function App() {
-  return <Home />;
+  // return <Home />;
+  return <Navbar />;
 }
 
 export default App;
