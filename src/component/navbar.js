2ffafa2437f7404c32fb2c10d2167d8ae20069dd
Sania Bansotra <saniabansotra@gmail.com>
2023-08-14 18:51:40 +0530

    message

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -34,22 +34,12 @@ const Navbar = () => {
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" href="#">
+              <a class="nav-link " href="#">
                 Disabled
               </a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
-          </form>
+          <form class="form-inline my-2 my-lg-0"></form>
         </div>
       </nav>
     </>
